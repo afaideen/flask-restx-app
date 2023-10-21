d9d00a5fb9e47af9c9d035a14e9c37ba697e8674
han <afaideen@gmail.com>
2023-10-21 17:57:00 +0800

    change course name and even able to delete data in db

--- app/resources.py
+++ app/resources.py
@@ -24,3 +24,24 @@ class CourseListAPI(Resource):
         db.session.add(course)
         db.session.commit()
         return course, 201
+
+    @ns.route("/courses/<int:id>")
+    class CourseAPI(Resource):
+        @ns.marshal_with(course_model)
+        def get(self, id):
+            course = Course.query.get(id)
+            return course
+
+        @ns.expect(course_input_model)
+        @ns.marshal_with(course_model)
+        def put(self, id):
+            course = Course.query.get(id)
+            course.name = ns.payload["name"]
+            db.session.commit()
+            return course
+
+        def delete(self, id):
+            course = Course.query.get(id)
+            db.session.delete(course)
+            db.session.commit()
+            return {}, 204
