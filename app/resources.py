5763bc43921ba760556403ac08173c93c2f6d7e7
han <afaideen@gmail.com>
2023-10-21 17:52:21 +0800

    db can add course name

--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,6 @@
 from flask_restx import Resource, Namespace
 
-# from .api_models import course_model, student_model, course_input_model, student_input_model
+from .api_models import course_model, student_model, course_input_model, student_input_model
 from .extensions import db
 from .models import Course, Student
 
@@ -13,14 +13,14 @@ class Hello(Resource):
 
 @ns.route("/courses")
 class CourseListAPI(Resource):
-    # @ns.marshal_list_with(course_model)
+    @ns.marshal_list_with(course_model)
     def get(self):
         return Course.query.all()
 
-    # @ns.expect(course_input_model)
-    # @ns.marshal_with(course_model)
-    # def post(self):
-    #     course = Course(name=ns.payload["name"])
-    #     db.session.add(course)
-    #     db.session.commit()
-    #     return course, 201
+    @ns.expect(course_input_model)
+    @ns.marshal_with(course_model)
+    def post(self):
+        course = Course(name=ns.payload["name"])
+        db.session.add(course)
+        db.session.commit()
+        return course, 201
