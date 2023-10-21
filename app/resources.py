9950eb2b565cd68df7c1bb5cd8134b794faa6d3e
han <afaideen@gmail.com>
2023-10-21 18:17:18 +0800

    api/v1

--- app/resources.py
+++ app/resources.py
@@ -4,7 +4,7 @@ from .api_models import course_model, student_model, course_input_model, student
 from .extensions import db
 from .models import Course, Student
 
-ns = Namespace("API Students and Courses")
+ns = Namespace("api/v1")
 
 @ns.route("/hello")
 class Hello(Resource):
