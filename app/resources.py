28b1be5e27d9406b77e40c6a99246e2a59b5f32d
han <afaideen@gmail.com>
2023-10-21 17:43:26 +0800

    api featuring querying database

--- app/resources.py
+++ app/resources.py
@@ -2,7 +2,7 @@ from flask_restx import Resource, Namespace
 
 # from .api_models import course_model, student_model, course_input_model, student_input_model
 from .extensions import db
-# from .models import Course, Student
+from .models import Course, Student
 
 ns = Namespace("api")
 
@@ -10,3 +10,17 @@ ns = Namespace("api")
 class Hello(Resource):
     def get(self):
         return {"hello": "restx"}
+
+@ns.route("/courses")
+class CourseListAPI(Resource):
+    # @ns.marshal_list_with(course_model)
+    def get(self):
+        return Course.query.all()
+
+    # @ns.expect(course_input_model)
+    # @ns.marshal_with(course_model)
+    # def post(self):
+    #     course = Course(name=ns.payload["name"])
+    #     db.session.add(course)
+    #     db.session.commit()
+    #     return course, 201
